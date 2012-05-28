ac53b2949cfaed6505ce126c0c84b3bbe30720b7
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-05-29 00:09:19 +0400

    Доработан jQuery плагин
    Вывод пейджера
    Единый метод для перехода между слайдами

--- jquery/script.js
+++ jquery/script.js
@@ -6,36 +6,69 @@
                 transitionDuration: 1000,
                 showPager: true
             }),
-            // step,
-            count,
-            pager,
             slide,
             t;
 
         this.init = function() {
             this.each(function() {
                 var obj = this,
-                    t;
+                    items = obj.children.length,
+                    pager;
+
+                if (settings.showPager) {
+                    pager = '<ul class="pager">';
+                    for (var i=0; i < items; i++) {
+                        pager += '<li></li>';
+                    }
+                    pager += '</ul>';
+
+                    obj.pager = $(pager).insertAfter($(obj).parent());
+
+                    obj.pager.children().eq(0).addClass('current');
+
+                    obj.pager.children().click(function() {
+                        var index = $(this).index();
+                        
+                        slide.apply(obj, [index]);
+                    });
+                }
 
                 t = setTimeout(function() {
                     slide.apply(obj);
-                }, settings.autoplay);
+                    }, settings.autoplay);
             });
             return this;
         };
 
-        slide = function() {
-            var obj = $(this),
-                step = obj.children().eq(0).width(),
-                count = obj.children().length,
-                coord = (parseInt(obj.css('left'), 10) - step) % (step * count),
-                t;
+        slide = function(index) {
+            var obj = this,
+                $obj = $(this),
+                step = $obj.children().eq(0).width(),
+                count = $obj.children().length,
+                coord;
+            
+            if(typeof(index) === 'undefined') {
+                coord = (parseInt($obj.css('left'), 10) - step) % (step * count);
+            } else {
+                coord = index * step * -1;
+            }
 
-            obj.animate({left: coord});
+            if(settings.showPager) {
+                position = Math.abs(coord) / step;
+
+                $(obj.pager).children().
+                    removeClass('current').
+                    eq(position).
+                    addClass('current');
+            }
+
+            $obj.animate({left: coord});
+            
+            clearTimeout(t);
 
             t = setTimeout(function() {
                 slide.apply(obj);
-            }, settings.autoplay);
+                }, settings.autoplay);
         };
 
         return this.init();
